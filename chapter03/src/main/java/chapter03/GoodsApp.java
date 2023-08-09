cd0396d272ca755328818974235b9a0349b32e5d
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-08-09 12:50:19 +0900

    필드/변수 finished

--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -4,16 +4,26 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Goods camera = new Goods();
-		camera.name = "Nikon";
-		camera.price = 400000;
-		camera.countStock = 10;
-		camera.countSold = 20;
+		camera.setName("Nikon");
+		camera.setPrice(400000);
+		camera.setCountStock(10);
+		camera.setCountSold(20);
+		
+		// 정보은닉(데이터보호)
+		camera.setPrice(-1);
+		
+		// countOfGoods 테스트
+		Goods goods1 = new Goods();
+		Goods goods2 = new Goods();
+		Goods goods3 = new Goods();
+		
+		System.out.println(Goods.countOfGoods);
 		
 		System.out.println(
-			"상품이름 : " + camera.name +
-			", 가격: " + camera.price +
-			", 재고개수:" + camera.countStock + 
-			", 팔린 개수: " + camera.countSold);
+			"상품이름 : " + camera.getName() +
+			", 가격: " + camera.getPrice() +
+			", 재고개수:" + camera.getCountStock() + 
+			", 팔린 개수: " + camera.getCountSold());
 	}
 
 }
