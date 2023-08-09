d3460c611063467c14347ef24a50754a772ef7b6
포스코DX-비트아카데미 <141541801+poscodx-bitacademy@users.noreply.github.com>
2023-08-09 16:23:37 +0900

    Add files via upload

--- practice02/src/main/java/prob02/Prob02.java
+++ practice02/src/main/java/prob02/Prob02.java
@@ -7,20 +7,21 @@ public class Prob02 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int[] intArray = new int[ 5 ];
-		double sum = 0;
+		int[] intArray = new int[5];
 
-		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
-		for(int i = 0; i < 5; i++) {
-			int n = scanner.nextInt();
+		System.out.println("5개의 숫자를 입력하세요.");
+		for(int i = 0; i < intArray.length; i++) {
+			int number = scanner.nextInt();
+			intArray[i] = number;
 		}
-		
-		
-		/* 배열에 저장된 정수 값 더하기 */
-		
-		/* 출력 */
-		
-		/* 자원정리 */
+
 		scanner.close();
+
+		double sum = 0.;
+		for(int i = 0; i < intArray.length; i++) {
+			sum += intArray[i];
+		}
+
+		System.out.println(sum / intArray.length);
 	}
 }
