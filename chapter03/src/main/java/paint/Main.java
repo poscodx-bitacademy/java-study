c949324159c07fb7c35fc483aa0fb4ae274886f1
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-08-10 12:32:17 +0900

    상속과 다형성(추상클래스, 인터페이스)

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -6,7 +6,8 @@ public class Main {
 		Point point1 = new Point(10, 10);
 //		point1.setX(10);
 //		point1.setY(10);
-		drawPoint(point1);
+		//drawPoint(point1);
+		draw(point1);
 		// point1.disappear();
 		point1.show(false);
 		
@@ -14,23 +15,46 @@ public class Main {
 		point2.setX(20);
 		point2.setY(20);
 		((ColorPoint)point2).setColor("red");
-		drawPoint(point2);
-
-		Rect rect = new Rect();
-		drawRect(rect);
+		//drawPoint(point2);
+		draw(point2);
 		
+		Rect rect = new Rect();
+		// drawRect(rect);
+		// drawShape(rect);
+		draw(rect);
+		
+		Triangle triangle = new Triangle();
+		// drawTriangle(triangle);
+		// drawShape(triangle);
+		draw(triangle);
+		
+		Circle circle = new Circle();
+		// drawShape(circle);
+		draw(circle);
+		
+		draw(new GraphicText("Hello World"));
 	}
 
-	public static void drawPoint(Point point) {
-		point.show(true);
+	public static void draw(Drawable drawable) {
+		drawable.draw();
 	}
+	
+//	public static void drawPoint(Point point) {
+//		point.show(true);
+//	}
 	
 //	public static void drawColorPoint(ColorPoint colorPoint) {
 //		colorPoint.show();
 //	}
+
+//	public static void drawShape(Shape shape) {
+//		shape.draw();
+//	}
 	
-	public static void drawRect(Rect rect) {
-		rect.draw();
-	}
-	
+//	public static void drawRect(Rect rect) {
+//		rect.draw();
+//	}
+//	public static void drawTriangle(Triangle triangle) {
+//		triangle.draw();
+//	}	
 }
--- chapter03/src/main/java/paint/Point.java
+++ chapter03/src/main/java/paint/Point.java
@@ -1,6 +1,6 @@
 package paint;
 
-public class Point {
+public class Point implements Drawable {
 	private int x;
 	private int y;
 	
@@ -40,5 +40,9 @@ public class Point {
 			System.out.println("점[x=" + x + ",y=" + y + "]을 지웠습니다");
 		}
 	}
-	
+
+	@Override
+	public void draw() {
+		show();
+	}
 }
