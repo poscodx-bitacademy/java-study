0a28bf23a2d36e65b5dc85202b49af39eff82b8a
포스코DX-비트아카데미 <141541801+poscodx-bitacademy@users.noreply.github.com>
2023-08-10 16:32:32 +0900

    Add files via upload

--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -6,23 +6,31 @@ public class GoodsApp {
 	private static final int COUNT_GOODS = 3;
 
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
 
 		Goods[] goods = new Goods[COUNT_GOODS];
 
-		// 상품 입력
-		String line = scanner.nextLine();
-		String[] infos = line.split(" ");
-		
-		String name = infos[0];
-		int price = Integer.parseInt(infos[1]);
-		int count = Integer.parseInt(infos[2]);
-		
-		System.out.println(name + ":" + price + ":" + count);
-		
-		// 상품 출력
-		
-		// 자원정리
+		for (int i = 0; i < COUNT_GOODS; i++) {
+			String line = scanner.nextLine();
+
+			String[] tokens = line.split(" ");
+			String name = tokens[0];
+			int price = Integer.parseInt(tokens[1]);
+			int countStock = Integer.parseInt(tokens[2]);
+
+			Goods g = new Goods();
+			g.setName(name);
+			g.setPrice(price);
+			g.setCountStock(countStock);
+
+			goods[i] = g;
+		}
+
+		for (Goods g : goods) {
+			System.out.println(g.getName() + "(가격:" + g.getPrice() + "원)이 " + g.getCountStock() + "개 입고 되었습니다.");
+		}
+
 		scanner.close();
 	}
 }
