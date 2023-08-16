69a87d5ce7f2344b1900d56749ac6f4bcb525fc1
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-08-16 11:52:25 +0900

    collection framework - 2

--- chapter04/src/main/java/chapter04/CalendarTest.java
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -6,8 +6,36 @@ public class CalendarTest {
 
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
+		printDate(cal);
 		
-
+		cal.set(Calendar.YEAR, 2023);
+		cal.set(Calendar.MONTH, 11); //12월(month-1)
+		cal.set(Calendar.DATE, 25);
+		printDate(cal);
+		
+		cal.set(2013, 12, 26);
+		cal.add(Calendar.DATE, 10000);
+		printDate(cal);
 	}
 
+	private static void printDate(Calendar cal) {
+		final String[] DAYS = {"일", "월", "화", "수", "목", "금", "토"};
+				
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH); // 0 ~ 11, +1
+		int date = cal.get(Calendar.DATE);
+		int day = cal.get(Calendar.DAY_OF_WEEK); // 1(일) ~ 7(토)
+		int hour = cal.get(Calendar.HOUR);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		
+		System.out.println(
+			year + "/" +
+			(month + 1) + "/" +
+			date + " " +
+			DAYS[day-1] + "요일 " +
+			hour + ":" +
+			minute + ":" +
+			second);		
+	}
 }
