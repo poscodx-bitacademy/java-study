40aaadc4aadb636e1b03b22ed0681eb3026af77f
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-08-17 09:40:49 +0900

    Generic Class & Method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,19 +2,37 @@ package prob01;
 
 
 public class Printer {
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String s) {
+//		System.out.println(s);
+//	}
+	
+
+
+	public int sum(Integer... nums) {
+		int s = 0;
+		for(Integer i : nums) {
+			s += i;
+		}
+		
+		return s;
 	}
 	
-	public void println(boolean b) {
-		System.out.println(b);
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.print(t);
+		}
 	}
-
-	public void println(double d) {
-		System.out.println(d);
-	}
-
-	public void println(String s) {
-		System.out.println(s);
-	}
+	
 }
