9522ce8e7ea4b5e8843ad8218e340328e32e14e7
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-08-18 12:46:49 +0900

    thread #1

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -22,40 +22,43 @@ public class EchoServer {
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT), 10);
 			log("starts...[port:" + PORT + "]");
 			
-			Socket socket = serverSocket.accept();
-			
-			try {
-				InetSocketAddress remoteInetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
-				String remoteHostAddress = remoteInetSocketAddress.getAddress().getHostAddress();
-				int remotePort = remoteInetSocketAddress.getPort();
-				log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
-				
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
-				
-				while(true) {
-					String data = br.readLine();
-					if(data == null) {
-						log("closed by client");
-						break;
-					}
-					
-					log("received:" + data);
-					pw.println(data);
-				}				
-			} catch(SocketException e) {
-				log("suddenly closed by client");
-			} catch (IOException e) {
-				log("error:" + e);
-			} finally {
+			while(true) {
+				Socket socket = serverSocket.accept();
 				try {
-					if(socket != null && !socket.isClosed()) {
-						socket.close();
+					InetSocketAddress remoteInetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+					String remoteHostAddress = remoteInetSocketAddress.getAddress().getHostAddress();
+					int remotePort = remoteInetSocketAddress.getPort();
+					log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
+					
+					PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+					BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+					
+					while(true) {
+						String data = br.readLine();
+						if(data == null) {
+							log("closed by client");
+							break;
+						}
+						
+						log("received:" + data);
+						pw.println(data);
+					}				
+				} catch(SocketException e) {
+					log("suddenly closed by client");
+				} catch (IOException e) {
+					log("error:" + e);
+				} finally {
+					try {
+						if(socket != null && !socket.isClosed()) {
+							socket.close();
+						}
+					} catch(IOException e) {
+						e.printStackTrace();
 					}
-				} catch(IOException e) {
-					e.printStackTrace();
 				}
 			}
+			
+
 			
 		} catch (IOException e) {
 			log("error:" + e);
