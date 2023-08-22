31a4097460abcd44ff3861f18918e215ae2d4843
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-08-22 10:35:01 +0900

    tcp alogorithm & socket options

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -17,6 +17,9 @@ public class TCPServer {
 			//1. Server Socket 생성
 			serverSocket = new ServerSocket();
 			
+			//1-1. FIN-WAIT -> TIME_WAIT 상태에서도 소켓 포트 할당이 가능하도록 하기 위해...
+			serverSocket.setReuseAddress(true);
+			
 			//2. 바인딩(Binding)
 			// Socket에 InetSocketAddress(IPAddresss + port)를 바인딩 한다.
 			// IPAddress: 0.0.0.0: 특정 호스트 IP에 바인딩 하지 않는다.
@@ -48,6 +51,14 @@ public class TCPServer {
 					
 					String data = new String(buffer, 0, readByteCount, "utf-8");
 					System.out.println("[server] received:" + data);
+					
+					
+					// SO_TIMEOUT 옵션 테스트용
+//					try {
+//						Thread.sleep(3000);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
 					
 					//6. 데이터 쓰기
 					os.write(data.getBytes("utf-8"));
